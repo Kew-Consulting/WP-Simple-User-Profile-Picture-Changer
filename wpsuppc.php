9efa11b248a4d32d1dfc5369541ccd513184a275
Daniel Echeverria <dan.echeverria@outlook.com>
2019-08-31 21:52:23 +0100

    Adds Initial Plugin File

---
+++ wpsuppc.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP Simple User Profile Picture Changer
+Plugin URI: https://kewconsulting.co.uk/wordpress-plugins/wp-simple-user-profile-picture-changer
+description: >-
+Adds simple functionality for WordPress admins that want to add/edit/delete User Profile Pictures on the WordPress Edit User page.
+Version: 1.0
+Author: Daniel Echeverria
+Author URI: https://www.linkedin.com/in/daniel-echeverria/
+License: MIT
+*/
